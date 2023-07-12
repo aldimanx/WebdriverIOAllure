e1f86753345f8e469e5365103ebcb33172acc6cf
aldiansyahN-kargo <aldiansyah.noverio@kargo.tech>
2023-07-13 01:30:14 +0700

    start

---
+++ test/specs/test1.js
@@ -0,0 +1,9 @@
+describe("Demo Tests", () => {
+  it("My 1st Test", async () => {
+    browser.url("https://google.com/");
+    browser.pause(2000);
+    await $('[name="q"]').setValue("WebdriverIO");
+    await $('button[type="submit"]').click();
+    browser.keys("Enter");
+  });
+});
