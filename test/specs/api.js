f23d74ef33b6156830823003c080bce56158be8f
aldimanx <noverio_rio@yahoo.com>
2023-08-02 02:58:09 +0700

    fixed

--- test/specs/api.js
+++ test/specs/api.js
@@ -6,10 +6,11 @@ const web = "https://reqres.in/"
 const page = randomizer.makeValueRange(1,2)
 const name = randomizer.makeName()
 const job = randomizer.makeJob()
+let id
 
 describe('API Testing', () => {
 
-    
+
     it('should fetch user data', async () => {
     // Make an API request using axios
     await axios.get(web+`api/users?page=${page}`).then((resp) => {
@@ -21,7 +22,8 @@ describe('API Testing', () => {
     let length = data.length
 
     expect(resp.status).to.eq(200);
-   for(let i = 0; i < length; i++){
+   for(let i = 0; i < length; i++)
+   {
         expect(data[i]).to.be.not.null
         expect(data[1]).to.have.property('id').and.to.be.a('number')
         expect(data[i]).to.have.property('email')
@@ -41,10 +43,12 @@ describe('API Testing', () => {
             expect(data.name).to.be.eq(name)
             expect(data.job).to.be.eq(job)
             expect(data.id).to.be.not.null
+            id = data.id
         })
     })
 
-
-
+    it('test get id from different function', async () => {
+        console.log(id)
+        })
 });
 
--- wdio.conf.js
+++ wdio.conf.js
@@ -123,7 +123,10 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec',['allure', {outputDir: 'allure-results'}]],
+    reporters: ['spec',['allure', {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: true,}]],
 
     
     //
