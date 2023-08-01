455ed6d2b591c412eefa9e24984b1361262089af
aldimanx <noverio_rio@yahoo.com>
2023-08-02 02:20:57 +0700

    test

--- support/randomizer.js
+++ support/randomizer.js
@@ -10,11 +10,22 @@ class Randomizer {
     //     } 
     makeNumberUnderMax(max){
         return Math.floor(Math.random() * max);
-      }
+    }
       
     makeValueRange(min, max) {
     return Math.floor(Math.random() * max) + min;
     }
+
+    makeJob(){
+      const { faker } = require('@faker-js/faker');
+      let randomJob = faker.person.jobTitle()
+      return randomJob
+  }
+    makeName(){
+      const { faker } = require('@faker-js/faker');
+      let randomName = faker.person.fullName();
+      return randomName
+  }
 }
 
 module.exports = new Randomizer()
--- test/specs/api.js
+++ test/specs/api.js
@@ -4,14 +4,16 @@ const randomizer = require('../../support/randomizer');
 
 const web = "https://reqres.in/"
 const page = randomizer.makeValueRange(1,2)
+const name = randomizer.makeName()
+const job = randomizer.makeJob()
 
 describe('API Testing', () => {
-  it('should fetch user data', async () => {
+    it('should fetch user data', async () => {
     // Make an API request using axios
     await axios.get(web+`api/users?page=${page}`).then((resp) => {
 
     // Log the response data
-     //   console.log("responysatasnfanf: ",resp.data)
+    console.log("respond test: ",resp.data.data)
     // Validate the response (you can keep the validations as before)
     const data = resp.data.data
     let length = data.length
@@ -22,9 +24,25 @@ describe('API Testing', () => {
         expect(data[1]).to.have.property('id').and.to.be.a('number')
         expect(data[i]).to.have.property('email')
    }
-    // Add more validations as needed
-  });
-})
+         });
+    })
+
+    it('should perform a POST request', async () => {
+        const postData = {
+            name: name,
+            job: job,
+        };
+        await axios.post(web+'api/users', postData).then(resp => {
+            let data = resp.data
+            console.log("hasilnya ini coy",name,job)
+
+            expect(data.name).to.be.eq(name)
+            expect(data.job).to.be.eq(job)
+            expect(data.id).to.be.not.null
+        })
+    })
+
+
 
 });
 
